ee03789550984457821b302fa63ae9625e7ee104
Anton Semenov <anta.semenov@gmail.com>
2018-05-06 12:41:18 +0300

    add data to call back

--- api/milestoneColumn.ts
+++ api/milestoneColumn.ts
@@ -1,5 +1,5 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda'
 
 export const moveCard: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
-  cb(null)
+  cb(null, { success: true })
 }
